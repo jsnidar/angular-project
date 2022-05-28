daa70aa79d2bfc4bcd70f8f0a74db7c286496d11
Jeremiah <jsnidar@gmail.com>
2022-05-28 09:11:52 -0600

    Create methods to clear and add ingredients

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, NgModule, Input } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @Output() handleAddIngredient = new EventEmitter<Ingredient>()
 
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addIngredient = (event: Event, name : string, amount : string) => {
+    
+    event.preventDefault()
+    this.handleAddIngredient.emit(new Ingredient(name , parseInt(amount)))
+    
+    amount = "0"
+    name = ""
+  }
+
+  clearIngredient(name : HTMLInputElement, amount : HTMLInputElement){
+    name.value = ""
+    amount.value = "0"
   }
 
 }
