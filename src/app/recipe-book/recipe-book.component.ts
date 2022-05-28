38b8a422a81eaf14b44b0325a442c222f8d0f976
Jeremiah <jsnidar@gmail.com>
2022-05-28 09:05:12 -0600

    Add detail and getSelected to get selected recipe

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeBookComponent implements OnInit {
 
+  @Output() detail : Recipe
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getSelected(recipe : Recipe){
+    this.detail = recipe
+  }
 }
