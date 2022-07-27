e202f5ed932dd8f2e3ad74a7c0a1711e97b3966b
Jeremiah <jsnidar@gmail.com>
2022-07-27 15:36:56 -0500

    Update to td form

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output, NgModule, Input } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, NgModule, Input, ViewChild, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -7,26 +9,62 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
   
+  @ViewChild('formData', { static: false }) formData: NgForm
+  
+  editMode = false
+  editedItemIndex : number
+  subscription : Subscription
+  editedIngredient: Ingredient
+
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
-    
+    this.subscription = this.shoppingListService.startedEditing.
+    subscribe((id : number) => {
+      this.editedItemIndex = id
+      this.editMode = true
+      this.editedIngredient = this.shoppingListService.getIngredient(id)
+
+      const { name, amount } = {...this.editedIngredient}
+      this.formData.setValue({
+        name: name,
+        amount: amount
+      })
+    })
   }
 
-  addIngredient = (event: Event, name : string, amount : string) => {
-    
-    event.preventDefault()
-    this.shoppingListService.onAddIngredient(new Ingredient(name, parseInt(amount)))
-    
-    amount = "0"
-    name = ""
+
+  onSubmit() {
+    //this is a method that calls other functions. It cannot be an arrow function
+    this.editMode ? this.updateIngredient() : this.addIngredient()
+    this.editMode = false
+    this.formData.reset()
   }
 
-  clearIngredient(name : HTMLInputElement, amount : HTMLInputElement){
-    name.value = ""
-    amount.value = "0"
+  addIngredient() {
+    const { name, amount } = this.formData.value
+
+    this.shoppingListService
+    .onAddIngredient(new Ingredient(name, +amount))
+  }
+
+  updateIngredient() {
+    const { name, amount } = this.formData.value
+
+    this.shoppingListService.onUpdateIngredient(
+      this.editedItemIndex, 
+      new Ingredient(name, +amount))
+  }
+
+  clearIngredient(){
+    this.editMode = false
+    this.formData.reset()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
